b7565a618772707c096052d87ed60eb5b377e60c
liliang0504@163.com <qq722618>
2018-11-13 22:51:57 +0800

    ...

--- zheng-shop/db/kf_shop.sql
+++ zheng-shop/db/kf_shop.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS `pro_product`;
+DROP TABLE IF EXISTS `shop_product`;
 
-CREATE TABLE `pro_product` (
+CREATE TABLE `shop_product` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键id',
   `name` varchar(100) NOT NULL COMMENT '商品名称',
   `price` decimal(21,6) NOT NULL COMMENT '价格',
@@ -10,6 +10,6 @@ CREATE TABLE `pro_product` (
 
 /*Data for the table `pro_product` */
 
-insert  into `pro_product`(`id`,`name`,`price`,`number`) values (1,'小米6X','1499.000000',1000);
-insert  into `pro_product`(`id`,`name`,`price`,`number`) values (2,'小米8 透明探索版','3699.000000',100);
-insert  into `pro_product`(`id`,`name`,`price`,`number`) values (3,'小米电视4C 50英寸','1999.990000',800);
+insert  into `shop_product`(`id`,`name`,`price`,`number`) values (1,'小米6X','1499.000000',1000);
+insert  into `shop_product`(`id`,`name`,`price`,`number`) values (2,'小米8 透明探索版','3699.000000',100);
+insert  into `shop_product`(`id`,`name`,`price`,`number`) values (3,'小米电视4C 50英寸','1999.990000',800);
